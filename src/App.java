7a3874b6255a37a4aadd150b7d624439566f4d2b
Juan Vacca <clanbs11.12@gmail.com>
2025-07-28 15:01:10 -0500

    Add objects (instantiate)

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Circulo circulo1 =  new Circulo("Azul", 8.0);
+
+        circulo1.calcularArea();
+        circulo1.mostrarInfo();
+
+        Rectangulo rectangulo1 = new Rectangulo("Rojo", 5.0, 3.0);
+
+        rectangulo1.calcularArea();
+        rectangulo1.mostrarInfo();
+
+
     }
 }
