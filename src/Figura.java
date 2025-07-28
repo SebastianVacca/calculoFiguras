6566a34219d6c0eef253dab7358662e0697363a1
Juan Vacca <clanbs11.12@gmail.com>
2025-07-28 14:59:44 -0500

    Add superClass

---
+++ src/Figura.java
@@ -0,0 +1,18 @@
+public abstract class Figura {
+    private String color;
+
+    public Figura(String color){
+        this.color = color;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public abstract Double calcularArea();
+    
+}
